426020bfc7600a77d46b372546c378224534fc2f
Bruno Carpio <bcarpio181@gmail.com>
2024-12-01 18:21:58 -0300

    add strict false to adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            strict: false,
+        }),
         paths: {
             base:
                 process.env.NODE_ENV === "production"
